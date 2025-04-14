d08c8f6ffe895f6e1db43920285b666b2b5626fc
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2025-04-14 09:43:07 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
+const Plus = document.getElementById("plus");
+const Minus = document.getElementById("minus");
+const Value = document.getElementById("value");
 
- 
- const offerEnds = " 5 nov 2025";
- 
- function countdown () {
-       const offerDate = new Date(offerEnds);
-       const currentDate = new Date();
-       
-       const totalSeconds = ( offerDate - currentDate ) / 1000;
+let value = 0;
 
-       const Days = Math.floor( totalSeconds / (24*60*60));
-       const Hours = Math.floor( totalSeconds % (3600*24) / 3600 );
-       const Minutes = Math.floor( totalSeconds % 3600 / 60 );
-       const Second  = Math.floor( totalSeconds % 60 );
 
-       document.getElementById("days").innerText = formatTime(Days);
-       document.getElementById("hours").innerText = formatTime(Hours);
-       document.getElementById("minutes").innerText = formatTime(Minutes);
-       document.getElementById("second").innerText = formatTime(Second);
+function Increase(){
+    value++;
+    if( value >= 11 ){
+        value = 10;
+        Plus.style.cursor ="alias";
+        Value.innerText = value;
+    }
+    else{
+        Value.innerText = value;
+    }
 }
-
-function formatTime(time){
-       return time < 10 ? (`0${time}`) : time
+function Decrease(){
+    value--;
+    if( value < 0 ){
+        value = 0;
+        Minus.style.cursor ="alias";
+        Value.innerText = value;
+    }
+    else {
+        Value.innerText = value;
+    }
 }
-
-
-countdown()
-setInterval(countdown,1000)
