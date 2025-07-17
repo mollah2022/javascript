475160928c646380ad35f2b3cd0909385e5599d8
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2025-07-17 21:48:07 +0600

    Add files via upload

--- i1.js
+++ i1.js
@@ -1,22 +1,13 @@
-function calculate (num1 = 0, num2 = 0, operation = '+'){
-    switch (operation) {
-        case '+':
-        return num1 + num2;
-        case '-':
-        return num1 - num2;
-        case '*':
-        return num1 * num2;
-        case '/':
-        if (num2 === 0) {
-            throw new Error("Division by zero is not allowed.");
-        }
-        return num1 / num2;
-        default:
-        throw new Error("Invalid operation. Supported operations: +, -, *, /");
-    }       
-}
+console.log("Hello Line Number-1");
+console.log("Hello Line Number-2");
 
-console.log(calculate(10, 5, '+')); // 15
-console.log(calculate(10, 5, '-')); // 5
-console.log(calculate(10, 5, '*')); // 50
-console.log(calculate(10, 5, '/')); // 2
+setTimeout ( () => {
+    console.log("Hello Line Number-3");
+},3000)
+console.log("Hello Line Number-4");
+
+setInterval( () => {
+    console.log("Hello Line Number-5");
+},3000)
+
+console.log("Hello Line Number-6");
