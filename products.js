9c94b73f0e62a385cab855764db5e420d34ab080
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2025-07-24 13:09:09 +0600

    Add files via upload

---
+++ products.js
@@ -0,0 +1,9 @@
+const products = [
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+  { title: "Samsung", id: 4 },
+  { title: "Walton", id: 5 },
+  { title: "Vivo", id: 6 },
+];
+export default products;
